c101aa8b6b93a972e1a6c9c7a6d401b2fac6a2dc
tunghv16 <34737310+tunghv16@users.noreply.github.com>
2020-02-16 23:51:53 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,23 +4,24 @@ $('#div-chat').hide();
 socket.on('listOnline', function (data) {
     $('#div-chat').show();
     $('#div-register').hide();
+    
     data.forEach(function (user) {
-        const { ten, peerId } = user;
-        $('.list-user').append(`<li id="${peerId}">${ten}</li>`);
+        const {ten, peerId} = user;
+        $('.list-user').append(`<li id="${ peerId }">${ ten }</li>`);
     });
     
-    socket.on('co_nguoi_moi', function (u) {
-        const { ten, peerId } = u;
-        $('.list-user').append(`<li id="${peerId}">${ten}</li>`);
+    socket.on('newUserLogin', function (u) {
+        const {ten, peerId} = u;
+        $('.list-user').append(`<li id="${ peerId }">${ ten }</li>`);
     });
     
-    socket.on('user_disconnect', function (peerId) {
-        $(`#${peerId}`).remove();
+    socket.on('userDisconnect', function (peerId) {
+        $(`#${ peerId }`).remove();
     })
 });
 
 socket.on('failed', function () {
-   alert('Da co nguoi dang ky ten nay roi');
+    alert('Tên này đã được sử dụng. Vui lòng đăng ký tên khác!!!');
 });
 
 $('.list-user').on('click', 'li', function () {
@@ -35,37 +36,40 @@ $('.list-user').on('click', 'li', function () {
 });
 
 function openStream() {
-    const config = { audio: true, video: true };
+    const config = {audio : true, video : true};
     
     return navigator.mediaDevices.getUserMedia(config);
 }
 
 function playStream(idVideoTag, stream) {
-    const video = document.getElementById(idVideoTag);
-     video.srcObject = stream;
-     video.play();
+    const video     = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
 }
 
 // openStream().then(stream => playStream('localStream', stream));
 
-const peer = new Peer({key: 'lwjd5qra8257b9'});
+const peer = new Peer({key : 'lwjd5qra8257b9'});
 
-peer.on('open', function(id) {
+peer.on('open', function (id) {
     $('#my-peer').append(id);
     
     $('#btnSignUp').on('click', function () {
         let userName = $('#txtUserName').val();
         
-        socket.emit('register', { ten:userName, peerId:id });
+        socket.emit('register', {ten : userName, peerId : id});
     });
 });
 
 // caller
 $('#btn-call').on('click', function () {
     const id = $('#remote-id').val();
+    
     openStream().then(function (strem) {
         playStream('localStream', strem);
+        
         const call = peer.call(id, strem);
+        
         call.on('stream', function (remoteStream) {
             playStream('remoteStream', remoteStream);
         })
@@ -73,11 +77,13 @@ $('#btn-call').on('click', function () {
 });
 
 // callee
-peer.on('call', function(call) {
+peer.on('call', function (call) {
     openStream().then(function (stream) {
         call.answer(stream); // Answer the call with an A/V stream.
+        
         playStream('localStream', stream);
-        call.on('stream', function(remoteStream) {
+        
+        call.on('stream', function (remoteStream) {
             playStream('remoteStream', remoteStream);
         });
     })
